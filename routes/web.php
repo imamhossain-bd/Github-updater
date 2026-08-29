41aefe239d3f53f2dd6b5a7474a182efed8c9764
imamhossain-bd <imamhossain0098@gmail.com>
2026-08-29 15:00:14 +0600

    Route update

--- routes/web.php
+++ routes/web.php
@@ -3,18 +3,11 @@
 use Illuminate\Support\Facades\Route;
 use Illuminate\Http\Request;
 
-Route::post('/github-webhook', function (Request $request) {
-    $secret = config('github-updater.webhook_secret');
-    $signature = $request->header('X-Hub-Signature-256');
-
-    if ($secret) {
-        $hash = 'sha256=' . hash_hmac('sha256', $request->getContent(), $secret);
-        if (!hash_equals($hash, $signature ?? '')) {
-            abort(403, 'Invalid signature');
-        }
-    }
-
+Route::get('/github-pull', function (Request $request) {
     \Artisan::call('app:update-from-github');
 
-    return response()->json(['status' => 'updated']);
+    return response()->json([
+        'status' => 'updated',
+        'output' => \Artisan::output(),
+    ]);
 });
